bbfb7e4f7b91c8d44a50ff537d3b46069e58e64b
griffin-stewie <panterathefamilyguy@gmail.com>
2022-05-26 19:59:27 +0900

    bump version

--- Sources/xopen/Commands/RootCommand.swift
+++ Sources/xopen/Commands/RootCommand.swift
@@ -8,7 +8,7 @@ struct RootCommand: AsyncParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "xopen",
         abstract: "Open file using Xcode version you defined by .xcode-version",
-        version: "1.7.0",
+        version: "1.8.0",
         subcommands: [
             DefaultOpenCommand.self,
             OpenCommand.self,
